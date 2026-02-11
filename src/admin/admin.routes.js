d9bc0a0898196ad810fa8836e12c6402fef59a16
FernanMaldonado <fmaldonado-2024021@kinal.edu.gt>
2026-02-10 18:10:35 -0600

    T24: Crear Rutas de adminstradores

---
+++ src/admin/admin.routes.js
@@ -0,0 +1,31 @@
+import { Router } from 'express';
+import {
+    getAdmins,
+    getAdminById,
+    createAdmin,
+    updateAdmin,
+    changeAdminStatus
+} from './admin.controller.js';
+
+import {
+    validateCreateAdmin,
+    validateUpdateAdmin,
+    validateAdminStatusChange,
+    validateGetAdminById
+} from '../../middlewares/admin-validation.js';
+
+const router = Router();
+
+// Rutas GET
+router.get('/', getAdmins);
+router.get('/:id', validateGetAdminById, getAdminById);
+
+// Rutas POST
+router.post('/', validateCreateAdmin, createAdmin);
+
+// Rutas PUT
+router.put('/:id', validateUpdateAdmin, updateAdmin);
+router.put('/:id/activate', validateAdminStatusChange, changeAdminStatus);
+router.put('/:id/deactivate', validateAdminStatusChange, changeAdminStatus);
+
+export default router;
