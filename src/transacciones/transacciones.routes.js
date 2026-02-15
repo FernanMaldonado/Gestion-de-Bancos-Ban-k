f47542877c1b502e014117565278a5a405cd012b
azacarias-dev <azacarias-2024007@kinal.edu.gt>
2026-02-15 07:55:53 -0600

    Se realizo la validacion de datos para crear una transaccion

--- src/transacciones/transacciones.routes.js
+++ src/transacciones/transacciones.routes.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import { getTransacciones, transaction } from './transacciones.controller.js';
+import { validateTransaccion } from '../../middlewares/transacciones-validation.js';
 
 const router = Router();
 
 // Ruta para realizar una transacción entre dos cuentas
-router.post('/', transaction)
+router.post('/', validateTransaccion, transaction)
 
 // Ruta para obtener todas las transacciones
 router.get('/', getTransacciones)
